939ba4b57f7c734809c39ff7978cc9b1d97b7afb
Joe Ganobsik <jganobsik@gmail.com>
2019-02-11 23:56:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function upperCaseCustomerName() {
 
 function setBestCustomer() {
   var bestCustomer = 'not bob';
+  bestCustomer
 }
 function overWriteBestCustomer() {
   bestCustomer = 'maybe bob';
